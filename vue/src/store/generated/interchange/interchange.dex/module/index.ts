527f8cc60288d3eb8b9719d31ac30bb8d2202772
leon <leon@ponyft.com>
2022-09-29 12:29:53 +0800

    packet sell-order amountDenom amount:int priceDenom price:int --ack remainingAmount:int,gain:int --module dex

--- vue/src/store/generated/interchange/interchange.dex/module/index.ts
+++ vue/src/store/generated/interchange/interchange.dex/module/index.ts
@@ -4,10 +4,12 @@ import { StdFee } from "@cosmjs/launchpad";
 import { SigningStargateClient } from "@cosmjs/stargate";
 import { Registry, OfflineSigner, EncodeObject, DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
 import { Api } from "./rest";
+import { MsgSendSellOrder } from "./types/dex/tx";
 import { MsgSendCreatePair } from "./types/dex/tx";
 
 
 const types = [
+  ["/interchange.dex.MsgSendSellOrder", MsgSendSellOrder],
   ["/interchange.dex.MsgSendCreatePair", MsgSendCreatePair],
   
 ];
@@ -41,6 +43,7 @@ const txClient = async (wallet: OfflineSigner, { addr: addr }: TxClientOptions =
 
   return {
     signAndBroadcast: (msgs: EncodeObject[], { fee, memo }: SignAndBroadcastOptions = {fee: defaultFee, memo: ""}) => client.signAndBroadcast(address, msgs, fee,memo),
+    msgSendSellOrder: (data: MsgSendSellOrder): EncodeObject => ({ typeUrl: "/interchange.dex.MsgSendSellOrder", value: MsgSendSellOrder.fromPartial( data ) }),
     msgSendCreatePair: (data: MsgSendCreatePair): EncodeObject => ({ typeUrl: "/interchange.dex.MsgSendCreatePair", value: MsgSendCreatePair.fromPartial( data ) }),
     
   };
