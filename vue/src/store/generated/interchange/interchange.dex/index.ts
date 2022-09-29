9afee12a660d094667a234889283a5b2a2e21e57
leon <leon@ponyft.com>
2022-09-29 12:31:10 +0800

    message cancel-sell-order port channel amountDenom priceDenom orderID:int --desc "Cancel a sell order" --module dex

--- vue/src/store/generated/interchange/interchange.dex/index.ts
+++ vue/src/store/generated/interchange/interchange.dex/index.ts
@@ -279,6 +279,21 @@ export default {
 		},
 		
 		
+		async sendMsgSendCreatePair({ rootGetters }, { value, fee = [], memo = '' }) {
+			try {
+				const txClient=await initTxClient(rootGetters)
+				const msg = await txClient.msgSendCreatePair(value)
+				const result = await txClient.signAndBroadcast([msg], {fee: { amount: fee, 
+	gas: "200000" }, memo})
+				return result
+			} catch (e) {
+				if (e == MissingWalletError) {
+					throw new Error('TxClient:MsgSendCreatePair:Init Could not initialize signing client. Wallet is required.')
+				}else{
+					throw new Error('TxClient:MsgSendCreatePair:Send Could not broadcast Tx: '+ e.message)
+				}
+			}
+		},
 		async sendMsgSendBuyOrder({ rootGetters }, { value, fee = [], memo = '' }) {
 			try {
 				const txClient=await initTxClient(rootGetters)
@@ -309,22 +324,35 @@ export default {
 				}
 			}
 		},
-		async sendMsgSendCreatePair({ rootGetters }, { value, fee = [], memo = '' }) {
+		async sendMsgCancelSellOrder({ rootGetters }, { value, fee = [], memo = '' }) {
 			try {
 				const txClient=await initTxClient(rootGetters)
-				const msg = await txClient.msgSendCreatePair(value)
+				const msg = await txClient.msgCancelSellOrder(value)
 				const result = await txClient.signAndBroadcast([msg], {fee: { amount: fee, 
 	gas: "200000" }, memo})
 				return result
 			} catch (e) {
 				if (e == MissingWalletError) {
-					throw new Error('TxClient:MsgSendCreatePair:Init Could not initialize signing client. Wallet is required.')
+					throw new Error('TxClient:MsgCancelSellOrder:Init Could not initialize signing client. Wallet is required.')
 				}else{
-					throw new Error('TxClient:MsgSendCreatePair:Send Could not broadcast Tx: '+ e.message)
+					throw new Error('TxClient:MsgCancelSellOrder:Send Could not broadcast Tx: '+ e.message)
 				}
 			}
 		},
 		
+		async MsgSendCreatePair({ rootGetters }, { value }) {
+			try {
+				const txClient=await initTxClient(rootGetters)
+				const msg = await txClient.msgSendCreatePair(value)
+				return msg
+			} catch (e) {
+				if (e == MissingWalletError) {
+					throw new Error('TxClient:MsgSendCreatePair:Init Could not initialize signing client. Wallet is required.')
+				} else{
+					throw new Error('TxClient:MsgSendCreatePair:Create Could not create message: ' + e.message)
+				}
+			}
+		},
 		async MsgSendBuyOrder({ rootGetters }, { value }) {
 			try {
 				const txClient=await initTxClient(rootGetters)
@@ -351,16 +379,16 @@ export default {
 				}
 			}
 		},
-		async MsgSendCreatePair({ rootGetters }, { value }) {
+		async MsgCancelSellOrder({ rootGetters }, { value }) {
 			try {
 				const txClient=await initTxClient(rootGetters)
-				const msg = await txClient.msgSendCreatePair(value)
+				const msg = await txClient.msgCancelSellOrder(value)
 				return msg
 			} catch (e) {
 				if (e == MissingWalletError) {
-					throw new Error('TxClient:MsgSendCreatePair:Init Could not initialize signing client. Wallet is required.')
+					throw new Error('TxClient:MsgCancelSellOrder:Init Could not initialize signing client. Wallet is required.')
 				} else{
-					throw new Error('TxClient:MsgSendCreatePair:Create Could not create message: ' + e.message)
+					throw new Error('TxClient:MsgCancelSellOrder:Create Could not create message: ' + e.message)
 				}
 			}
 		},
--- vue/src/store/generated/interchange/interchange.dex/module/index.ts
+++ vue/src/store/generated/interchange/interchange.dex/module/index.ts
@@ -4,15 +4,17 @@ import { StdFee } from "@cosmjs/launchpad";
 import { SigningStargateClient } from "@cosmjs/stargate";
 import { Registry, OfflineSigner, EncodeObject, DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
 import { Api } from "./rest";
+import { MsgSendCreatePair } from "./types/dex/tx";
 import { MsgSendBuyOrder } from "./types/dex/tx";
 import { MsgSendSellOrder } from "./types/dex/tx";
-import { MsgSendCreatePair } from "./types/dex/tx";
+import { MsgCancelSellOrder } from "./types/dex/tx";
 
 
 const types = [
+  ["/interchange.dex.MsgSendCreatePair", MsgSendCreatePair],
   ["/interchange.dex.MsgSendBuyOrder", MsgSendBuyOrder],
   ["/interchange.dex.MsgSendSellOrder", MsgSendSellOrder],
-  ["/interchange.dex.MsgSendCreatePair", MsgSendCreatePair],
+  ["/interchange.dex.MsgCancelSellOrder", MsgCancelSellOrder],
   
 ];
 export const MissingWalletError = new Error("wallet is required");
@@ -45,9 +47,10 @@ const txClient = async (wallet: OfflineSigner, { addr: addr }: TxClientOptions =
 
   return {
     signAndBroadcast: (msgs: EncodeObject[], { fee, memo }: SignAndBroadcastOptions = {fee: defaultFee, memo: ""}) => client.signAndBroadcast(address, msgs, fee,memo),
+    msgSendCreatePair: (data: MsgSendCreatePair): EncodeObject => ({ typeUrl: "/interchange.dex.MsgSendCreatePair", value: MsgSendCreatePair.fromPartial( data ) }),
     msgSendBuyOrder: (data: MsgSendBuyOrder): EncodeObject => ({ typeUrl: "/interchange.dex.MsgSendBuyOrder", value: MsgSendBuyOrder.fromPartial( data ) }),
     msgSendSellOrder: (data: MsgSendSellOrder): EncodeObject => ({ typeUrl: "/interchange.dex.MsgSendSellOrder", value: MsgSendSellOrder.fromPartial( data ) }),
-    msgSendCreatePair: (data: MsgSendCreatePair): EncodeObject => ({ typeUrl: "/interchange.dex.MsgSendCreatePair", value: MsgSendCreatePair.fromPartial( data ) }),
+    msgCancelSellOrder: (data: MsgCancelSellOrder): EncodeObject => ({ typeUrl: "/interchange.dex.MsgCancelSellOrder", value: MsgCancelSellOrder.fromPartial( data ) }),
     
   };
 };
